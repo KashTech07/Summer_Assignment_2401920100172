8d4f3dde15f3ba2a08dfba571c6960809e69bc9a
Kashish Sharma <kashishsuneetasharma@gmail.com>
2026-06-03 11:56:17 +0530

    Create Two_sum.java

---
+++ week_1/Day_1/Two_sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+Arrays.sort(nums);
+int i =0;
+int j=nums.length-1;
+while(i<j){
+    if(nums[i]+nums[j]==target){
+        return new int[]{i,j};
+    }
+    else if(nums[i]+nums[j]>target){
+        j--;
+    }
+    else{
+        i++;
+    }
+}
+return new int[]{-1,-1};}}
