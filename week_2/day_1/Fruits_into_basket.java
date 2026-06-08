440851594d2c956c8a4bec48fa26aea78e73eb0c
Kashish Sharma <kashishsuneetasharma@gmail.com>
2026-06-08 11:21:54 +0530

    Create Fruits_into_basket.java

---
+++ week_2/day_1/Fruits_into_basket.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int j = 0 ;
+        int count = 0;
+        HashMap<Integer,Integer> map = new HashMap<>() ;
+        for(int i = 0 ; i < fruits.length ; i ++){
+            map.put(fruits[i] , map.getOrDefault(fruits[i] , 0)+1) ;
+            while(map.size()>2){
+                map.put(fruits[j] , map.get(fruits[j])-1) ;
+                if(map.get(fruits[j])==0) map.remove(fruits[j])  ;
+                j++ ;
+            }
+            count=Math.max(i-j+1 , count) ;
+        }
+        return count ;}}
