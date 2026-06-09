c7ad568dccacf1415cd1affc7bf757f4023ef44e
Kashish Sharma <kashishsuneetasharma@gmail.com>
2026-06-09 11:00:11 +0530

    Create Sbarray_Sum_equals_k.java

---
+++ week_2/day_2/Sbarray_Sum_equals_k.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+       int count = 0 ;
+        int sum = 0;
+        HashMap<Integer , Integer> map = new HashMap<>() ;
+        map.put(0,1) ;
+        for(int i = 0 ; i<nums.length ; i++){
+            sum+=nums[i] ;
+            int c = sum-k ;
+            if(map.containsKey(c)){
+                count+=map.get(c) ;
+            }
+                map.put(sum , map.getOrDefault(sum,0)+1) ;
+            
+        }
+        return count ;}}
